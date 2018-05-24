18f1c15f64a14c03a1814be04b4b32e62a70c337
Quentin JACOB <qjacob@e3r6p6.42.fr>
2018-05-24 16:41:11 +0200

    correct the path

--- srcs/main_test/main_cat.c
+++ srcs/main_test/main_cat.c
@@ -6,11 +6,11 @@ int	main()
 {
 	int		fd;
 
-	fd = open("../Makefile", O_RDONLY);
+	fd = open("Makefile", O_RDONLY);
 	extern void ft_cat(int fd);
 	ft_cat(fd);
 	close(fd);
-	fd = open("../main_test", O_RDONLY);
+	fd = open("main_test", O_RDONLY);
 	ft_cat(fd);
 	close(fd);
 	return (0);
